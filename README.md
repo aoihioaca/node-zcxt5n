670c374f3a458693f98ca2a19e87b415a8ab43f6
aoihioaca <116234630+aoihioaca@users.noreply.github.com>
2023-04-21 13:43:14 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-zcxt5n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-zcxt5n)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<section>
+  <h1>川崎工科高校</h1>
+    <p>入学者募集中！</p>
+    <button type="">aaa</button>
+    <div style="display: none">
+      <img
+        width="100px"
+        src="https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcTRWThKzvURYqelTtB6BVn_TodOjfpX-3Hg0uv3zFq7X7Sw8zVjzNum_U8yrl0yxnNT"
+      />
+      <img
+        src="https://ichef.bbci.co.uk/news/800/cpsprodpb/B22C/production/_122521654_caithreesquirrelspic.jpg"
+        width="100px"
+      />
+    </div>
+    <img
+      width="300px"
+      src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Kawasaki_Koka.jpg"
+    />
+    <br/>
+  鹿島田駅.平間駅.各駅１０分　自転車3分　 <br>
+\wikipedia\commons\7\7a\Kawasaki_.jpgKoka<br>
+</section>
+
+<style>
+</style>
